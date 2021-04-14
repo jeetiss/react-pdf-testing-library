6bc253e2f85bd7cd2dba2130df8608cbcadbf366
jeetiss <jeetiss@yandex.ru>
2021-04-14 15:07:53 +0300

    optimize assemblyscript code

--- src/crop/assembly/index.ts
+++ src/crop/assembly/index.ts
@@ -1,34 +1,27 @@
-// The entry file of your WebAssembly module.
-
-export function crop(arr: Uint8ClampedArray, width: u32, height: u32): Array<u32> {
-  const result: Array<u32> = new Array<u32>(4);
-
-  // top
-  result[0] = height;
-  // right
-  result[1] = 0;
-  // bottom
-  result[2] = 0;
-  // left
-  result[3] = width;
+export function crop(
+  arr: Uint8ClampedArray,
+  width: u32,
+  height: u32
+): StaticArray<u32> {
+  // [top, right, bottom, left]
+  let result: StaticArray<u32> = [height, 0, 0, width];
 
   for (let y: u32 = 0; y < height; ++y) {
     for (let x: u32 = 0; x < width; ++x) {
       const pos: u32 = (y * width + x) * 4;
 
       if (
-        arr[pos] !== 255 ||
-        arr[pos + 1] !== 255 ||
-        arr[pos + 2] !== 255
+        unchecked(arr[pos]) !== 255 ||
+        unchecked(arr[pos + 1]) !== 255 ||
+        unchecked(arr[pos + 2]) !== 255
       ) {
-        // top
-        result[0] = min(result[0], y);
-        // right
-        result[1] = max(x, result[1]);
-        // bottom
-        result[2] = max(result[2], y);
-        // left
-        result[3] = min(result[3], x);
+        // [top, right, bottom, left]
+        result = [
+          min(unchecked(result[0]), y),
+          max(x, unchecked(result[1])),
+          max(unchecked(result[2]), y),
+          min(unchecked(result[3]), x),
+        ];
       }
     }
   }
@@ -36,4 +29,4 @@ export function crop(arr: Uint8ClampedArray, width: u32, height: u32): Array<u32
   return result;
 }
 
-export const dataId = idof<Uint8ClampedArray>()
+export const arrayPtr = idof<Uint8ClampedArray>();
--- src/crop/index.js
+++ src/crop/index.js
@@ -1,17 +1,20 @@
-import { instantiate } from '@assemblyscript/loader'
+import { instantiateStreaming } from '@assemblyscript/loader'
 import { readFile } from 'fs/promises'
 
-const asm = instantiate(readFile('./src/crop/assembly/code.wasm'))
+const asm = instantiateStreaming(readFile('./src/crop/assembly/code.wasm'))
 
 const clamp = (min, value, max) => Math.min(max, Math.max(value, min))
 
 export const crop = async (imageData, padding = 20) => {
   const { exports } = await asm
-  const { crop, dataId } = exports
-  const { __newArray, __getUint32Array } = exports
+  const { __newArray, __getArray } = exports
 
-  const [top, right, bottom, left] = __getUint32Array(
-    crop(__newArray(dataId, imageData.data), imageData.width, imageData.height)
+  const [top, right, bottom, left] = __getArray(
+    exports.crop(
+      __newArray(exports.arrayPtr, imageData.data),
+      imageData.width,
+      imageData.height
+    )
   )
 
   return {
